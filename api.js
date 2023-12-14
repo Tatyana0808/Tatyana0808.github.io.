01e7efb689b4d549bcc7587cc1f49caf21005822
Tatyana0808 <144019586+Tatyana0808@users.noreply.github.com>
2023-12-14 08:17:08 +0300

    Add files via upload

---
+++ api.js
@@ -0,0 +1,144 @@
+// Замени на свой, чтобы получить независимый от других набор данных.
+// "боевая" версия инстапро лежит в ключе prod
+import {renderApp, setPosts} from "./index.js";
+import { replaceSave } from "./helpers.js"
+//import { posts } from "./index.js";
+const personalKey = "vorobyeva-tatyana";
+const baseHost = "https://wedev-api.sky.pro";
+const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
+
+export function getPosts({ token }) {
+  return fetch(postsHost, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      if (response.status === 401) {
+        throw new Error("Нет авторизации");
+      }
+
+      return response.json();
+    })
+    .then((data) => {
+      return data.posts;
+    });
+}
+
+// https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/user/README.md#%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F
+export function registerUser({ login, password, name, imageUrl }) {
+  return fetch(baseHost + "/api/user", {
+    method: "POST",
+    body: JSON.stringify({
+      login,
+      password,
+      name,
+      imageUrl,
+    }),
+  }).then((response) => {
+    if (response.status === 400) {
+      throw new Error("Такой пользователь уже существует");
+    }
+    return response.json();
+  });
+}
+
+export function loginUser({ login, password }) {
+  return fetch(baseHost + "/api/user/login", {
+    method: "POST",
+    body: JSON.stringify({
+      login,
+      password,
+    }),
+  }).then((response) => {
+    if (response.status === 400) {
+      throw new Error("Неверный логин или пароль");
+    }
+    return response.json();
+  });
+}
+
+// Загружает картинку в облако, возвращает url загруженной картинки
+export function uploadImage({ file }) {
+  const data = new FormData();
+  data.append("file", file);
+
+  return fetch(baseHost + "/api/upload/image", {
+    method: "POST",
+    body: data,
+  }).then((response) => {
+    return response.json();
+  });
+}
+export function addPost({ token, imageUrl }) {
+  const commentInputElement = document.getElementById('description')
+  console.log( commentInputElement)
+  return fetch(postsHost, {
+      method: 'POST',
+      body: JSON.stringify({
+         
+          description: commentInputElement.value,
+          imageUrl,
+      }),
+      headers: {
+          Authorization: token,
+      },
+  }).then((response) => {
+      if (response.status === 400) {
+          alert('Нет фото или описания')
+      } else {
+          return response.json()
+      }
+  })
+}
+export function getPostsOfUser({ token, userId }) {
+  return fetch(`${postsHost}/user-posts/${userId}`, {
+      method: 'GET',
+      headers: {
+          Authorization: token,
+      },
+  })
+      .then((response) => {
+          if (response.status === 401) {
+              throw new Error('Нет авторизации')
+          }
+          return response.json()
+      })
+      .then((data) => {
+          setPosts(data.posts)
+          return data.posts
+      })
+      .catch((error) => {
+          alert('Кажется, у вас сломался интернет, попробуйте позже')
+          console.warn(error)
+      })
+}
+export function addLikePost({ token, postId }) {
+  return fetch(`${postsHost}/${postId}/like`, {
+      method: 'POST',
+      headers: {
+          Authorization: token,
+      },
+  }).then((response) => {
+      if (response.status === 401) {
+          alert('Лайкать посты могут только авторизованные пользователи')
+          throw new Error('Нет авторизации')
+      }
+      return response.json()
+  })
+}
+export function removeLikePost({ token, postId }) {
+  return fetch(`${postsHost}/${postId}/dislike`, {
+      method: 'POST',
+      headers: {
+          Authorization: token,
+      },
+  }).then((response) => {
+      if (response.status === 401) {
+          alert('Войдите, чтобы убрать лайк')
+          throw new Error('Нет авторизации')
+      }
+      return response.json()
+  })
+}
